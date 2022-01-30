f2bb926f6f99aff809c9e93e2f627b1e11b3d0d3
Cory <co.ry@icloud.com>
2022-01-30 14:01:16 -0500

    working code

--- caesar.rb
+++ caesar.rb
@@ -1,21 +1,26 @@
 
 def translate(message, shift, result = '')
+  phrase = []
   message.each_char do |char|  
     if char.ord.between?(65, 90)
       result = char.ord + shift
         if result > 90
-        result += -24
+        result += -26
         end
-    else if char.ord.between?(97, 122) 
+      else if char.ord.between?(97, 122) 
       result = char.ord + shift
      if result > 122
-      result += -24
+      result += -26
       end
+    else 
+      result = char.ord
     end
     end
-    puts result.chr
+      phrase.push(result.chr)
   end
+  puts phrase.join("")
 end
 
 
-translate("abc", 1)
+translate("What a string!", 5)
+
