2ea0ef0997d5f010632f1477032923c542995a1d
Cory <co.ry@icloud.com>
2022-01-30 13:43:59 -0500

    logic update

--- caesar.rb
+++ caesar.rb
@@ -1,20 +1,21 @@
-class CaesarCipher
-    def translate(message, shift, result = '')
-      message.each_char do |char|  
-        if char.ord.between?(65, 90)
-        result = char.ord + shift
-          if result > 90
-          result += -24
-          end
-        else if char.ord.between?(97, 122)
+
+def translate(message, shift, result = '')
+  message.each_char do |char|  
+    if char.ord.between?(65, 90)
       result = char.ord + shift
         if result > 90
-          result += -24
+        result += -24
         end
-        end
+    else if char.ord.between?(97, 122) 
+      result = char.ord + shift
+     if result > 122
+      result += -24
       end
-      puts result
     end
+    end
+    puts result.chr
   end
-  
-  to_num("Cory is the Best", 1)
+end
+
+
+translate("abc", 1)
